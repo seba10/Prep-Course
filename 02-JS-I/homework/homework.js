5e3ba7cffcfdac0c5cde9dbadb469ca35a54c871
seba10 <sebastianmansilla222@gmail.com>
2022-03-07 22:14:50 -0300

    tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -93,27 +93,39 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código: 
-
+  if (num > 50){
+    return true
+  } else {
+    return false
+  }
 }
 
 function obtenerResto(x, y) {
 // Obten el resto de la división de "x" entre "y"
   // Tu código:
-
+return (x % y)
 }
   
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 0) {
+    return true
+  }else {
+    return false
+  }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 !== 0) {
+    return true
+  }else {
+    return false
+  }
 }
 
 function elevarAlCuadrado(num) {
