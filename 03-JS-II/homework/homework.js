4308d9580b313b62a4d778fa87773fc0c2f43a90
seba10 <sebastianmansilla222@gmail.com>
2022-03-10 12:01:36 -0300

    tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,19 +66,28 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  
+  switch (color) {
+    case "blue": return "This is blue"
+    case "red": return "This is red"
+    case "green": return "This is green"
+    case "orange": return "This is orange"
+    default: return "Color not found"
+  }
+    
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (numero === 10 || numero === 5)
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (numero < 50 && numero > 20)
 }
 
 function esEntero(numero) {
@@ -89,6 +98,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+   
 }
 
 function fizzBuzz(numero) {
